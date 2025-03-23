f222ccdc808330dbd625080f022737ca8980a8bf
Erick-Ngooh <erickngooh@gmail.com>
2025-03-23 12:52:07 +0100

    feat: create raw socket for ICMP

--- src/ft_ping.c
+++ src/ft_ping.c
@@ -1,10 +1,25 @@
-#include <stdio.h>
+#include <stdio.h>              // printf, perror
+#include <sys/socket.h>         // socket()
+#include <netinet/in.h>         // AF_INET
+#include <netinet/ip_icmp.h>    // IPPROTO_ICMP
+#include <errno.h>              // errno
+#include <unistd.h>             // close()
 #include "ft_ping.h"
 
 int run_ping(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
-    printf("ft_ping started\n");
+
+    // Crée un socket RAW ICMP (AF_INET pour IPv4, SOCK_RAW pour brut, ICMP)
+    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (sockfd < 0)
+    {
+        perror("❌ Error: Cannot open raw socket");
+        return 1;
+    }
+
+    printf("✅ Socket created successfully (fd = %d)\n", sockfd);
+    close(sockfd);
     return 0;
 }
--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include "../include/ft_ping.h"
 
 int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
     run_ping(argc, argv);
     return 0;
 }
